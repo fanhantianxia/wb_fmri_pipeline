8a6f9c6af57da3b34f6c9eb49ad404138f19734d
test_matlab <51116957+fanhantianxia@users.noreply.github.com>
2019-09-09 22:38:46 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -5,14 +5,14 @@ combs_project_id=$3
 
 mkdir -p /app_file
 cd /app_file
-git clone https://github.com/fanhantianxia/GitHub.git /app_file
+git clone https://github.com/fanhantianxia/wb_fmri_pipeline_tool.git /app_file
 mkdir /root/matlab_script/
 cp /app_file/wb_pipeline_FCD /root/matlab_script/
+cp /app_file/wb_pipeline_FOCA /root/matlab_script/
 
 mkdir -p /file_buf
 cd /file_buf
-git clone https://github.com/fanhantianxia/wb_pipeline_FCD.git /file_buf
-
+git clone https://github.com/fanhantianxia/wb_fmri_pipeline.git /file_buf
 
 echo 'display Input_file_TreeGraph'
 cd /data
@@ -37,18 +37,21 @@ sleep 3s
 mkdir /DataBuf 
 fmriprep /data/data_BIDS $2 participant -w /DataBuf --no-submm-recon --fs-no-reconall
 
-mkdir /FCD  
-find /out/fmriprep -type f -name "*desc-preproc_bold.nii.gz" | xargs cp -t  /FCD
-gzip  -d  /FCD/*_bold.nii.gz
+mkdir /nit  
+find /out/fmriprep -type f -name "*desc-preproc_bold.nii.gz" | xargs cp -t  /nit
+gzip  -d  /nit/*_bold.nii.gz
 
 #在/FCD里添加config.json和fmriprep2FCD.py
-cp /file_buf/config.json /FCD
-cp /file_buf/fmriprep2FCD.py /FCD
-cd /FCD
-python fmriprep2FCD.py  #需要修改一下程序内部路径
+cp /file_buf/config.json /nit
+cp /file_buf/fmriprep2nit.py /nit
+cd /nit
+python fmriprep2nit.py  #需要修改一下程序内部路径
 
 chmod 777 -R /root/matlab_script/
-mkdir /out/FCD  #FCD输出文件夹0
-/root/matlab_script/wb_pipeline_FCD /FCD/FCD_Input /out/FCD /file_buf/brain_mask.nii 0.6 2
+mkdir /out/FCD  #FCD输出文件夹
+/root/matlab_script/wb_pipeline_FCD /nit/FCD_Input /out/FCD /file_buf/brain_mask.nii 0.6 2 0 #[Thresold] [TR] [ConnectType]
 
+mkdir /out/FOCA  #FOCA输出文件夹
+/root/matlab_script/wb_pipeline_FOCA /nit/FCD_Input /out/FOCA /file_buf/brain_mask.nii 2
 echo 'END'
+
