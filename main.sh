cda6ca5d4cec26854e717957c9c4ec25dc946bdc
test_matlab <51116957+fanhantianxia@users.noreply.github.com>
2019-09-10 20:46:41 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -37,6 +37,12 @@ sleep 3s
 mkdir /DataBuf 
 fmriprep /data/data_BIDS $2 participant -w /DataBuf --no-submm-recon --fs-no-reconall
 
+#matlab config
+env LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/MATLAB/MATLAB_Runtime/v90/runtime/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/bin/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/os/glnxa64:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64/native_threads:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64/server:/usr/local/MATLAB/MATLAB_Runtime/v90/sys/java/jre/glnxa64/jre/lib/amd64
+env XAPPLRESDIR=/usr/local/MATLAB/MATLAB_Runtime/v90/X11/app-defaults
+env MCR_CACHE_VERBOSE=true
+env MCR_CACHE_ROOT=/tmp
+
 mkdir /nit  
 find /out/fmriprep -type f -name "*desc-preproc_bold.nii.gz" | xargs cp -t  /nit
 gzip  -d  /nit/*_bold.nii.gz
